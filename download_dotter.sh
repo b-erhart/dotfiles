4e869a2e53a147164849e490d012c57e5fb83fbe
b-erhart <contact@b-erhart.de>
2022-12-26 19:18:44 +0100

    Add comments to download_dotter.sh

--- download_dotter.sh
+++ download_dotter.sh
@@ -2,6 +2,11 @@
 
 # DOWNLOAD DOTTER
 
+# Enter directory of this script
 cd -- "$(dirname -- "$0")" || ( echo "Failed to enter dotfiles directory. Abort" && exit )
+
+# Remove the dotter executable if it already exists
 [ -f ./dotter ] && rm ./dotter
+
+# Download the latest dotter executable
 wget --no-verbose --show-progress "https://github.com/SuperCuber/dotter/releases/latest/download/dotter"
